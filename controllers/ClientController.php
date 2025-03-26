bb3cf30e33a4d46d595c54d97ba2b12b9d3b73e9
longdo1307 <dolong.130703@gmail.com>
2025-03-26 18:56:34 +0700

    coltroler

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -51,6 +51,93 @@ class ClientController
                         echo "Lỗi khi lưu tệp ảnh.";
                         return;
                     }
-                }}}}
-
+                }
+            
+                   // Cập nhật thông tin
+                   $current_img = $_POST['current_img'];
+                   if ($this->modelClients->updateAccout($id, $username, $email, $avatar ?: $current_img, $address, $sdt)) {
+                       // Cập nhật thành công
+                       // Lấy lại thông tin người dùng mới
+                       $_SESSION['user'] = $this->modelClients->getAccountById($id);
+                       header('Location: http://localhost/base_test_DA1/public/');
+                       exit;
+                   } else {
+                       echo "Lỗi khi cập nhật tài khoản.";
+                   }
+               } else {
+                   require '../views/Clients/UpdateTaiKhoan/updateTk.php';
+                   exit;
+               }
+           }
+       
+           // Gọi view cập nhật tài khoản
+           require_once '../views/Clients/UpdateTaiKhoan/updateTk.php';
+       } 
+         
+       
+       public function login() {
+           require_once '../views/Clients/accounts/login.php';
+       }
+       public function postLogin() {
+           if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+               $userOrEmail = $_POST['username']; 
+               $pass = $_POST['password'];
+               $check = $this->modelClients->checkAcc($userOrEmail, $pass);
+               if (is_array($check)) {
+                   if($check['active'] === 1){
+                       echo '<script>
+                       // Khắc phục lỗi mất thanh cuộn
+                       document.body.style.overflowX = "auto"; 
+                       document.body.style.overflowY = "auto";  
+                       Swal.fire({
+                           text: "Tài khoản của bạn đã bị khoá",
+                           icon: "error",
+                           confirmButtonColor: "#C62E2E"
+                           });
+                     </script>';
+                     require_once '../views/Clients/accounts/login.php';
+                   }else{
+                       $_SESSION['user'] = $check;
+                       echo '<script>
+                       // Khắc phục lỗi mất thanh cuộn
+                       document.body.style.overflowX = "auto"; 
+                       document.body.style.overflowY = "auto";  
+                       Swal.fire({
+                           text: "Đăng nhập thành công",
+                           icon: "success",
+                           confirmButtonColor: "#C62E2E"
+                           });
+                     </script>';
+                       header('Location: http://localhost/base_test_DA1/public/');
+                       exit;
+                   }
+               } else {
+                   $thongbao = 'Tài khoản hoặc mật khẩu không đúng';
+               }
+           } else {
+               echo 'Lỗi';
+           }
+           require_once '../views/Clients/accounts/login.php';
+       }
+   
+       public function logOut(){
+           session_unset();
+           session_destroy();
+           header('Location: http://localhost/base_test_DA1/public/');
+       }
+       public function signUp() {
+           require_once '../views/Clients/accounts/signUp.php';
+       }
+       // Đăng kí tài khoản
+       public function addAccount(){
+           if($_SERVER['REQUEST_METHOD'] === 'POST'){
+               $username = $_POST['username'];
+               $email = $_POST['email'];
+               $sdt = $_POST['sdt'];
+               $password = $_POST['password'];
+               if($this->modelClients->addAccount($username, $email, $password, $sdt)){
+                   header('location: http://localhost/base_test_DA1/public/');
+               }
+           }
+       }
             
